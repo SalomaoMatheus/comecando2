f75ee57e64a32e13aee658c89b1c49ae7fbc03c9
Matheus Salomão <ms019042@gmail.com>
2021-05-11 22:07:42 -0300

    find distance tutorial

---
+++ Assets/Scenes/StartScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour {
+
+    void Start() {
+        float distance = getDistanceBetweenTwoPoints(0 , 10, 20, 30);
+        Debug.Log(distance);
+    }
+
+    float getDistanceBetweenTwoPoints(float x1, float y1, float x2, float y2 ) {
+        float dX = x2 - x1;
+        float dY = y2 - y2;
+        float distanceSquared = dX * dX + dY * dY;
+        float distance = Mathf.Sqrt(distanceSquared);
+        return distance;
+    }
+}
